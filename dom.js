2379f7204d84ca109ef0f15fc9f9819ad1e9c2c0
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-03 14:18:52 -0500

    Created dom.js; placed DOM elements in variables
    All elements that will be changed dynamically in JS have been placed in variables. Event listeners have been added where necessary. Callback functions can be changed if needed.

---
+++ dom.js
@@ -0,0 +1,24 @@
+
+// DOM handlers
+
+	// Header elements
+		 var userInput = document.getElementById("userInput");	// Grabs user text input field
+		 userInput.addEventListener("keyup", validateKeyup);		// Listens for keyup; runs function to validate for return key
+
+		 var darkTheme = document.getElementById("darkTheme");	// Grabs dark theme checkbox
+		 darkTheme.addEventListener("change", toggleDark);			// Listens for change; runs function to add/remove dark theme class
+
+		 var largeText = document.getElementById("largeText");	// Grabs large text checkbox
+		 largeText.addEventListener("change", toggleLarge);			// Listens for change; runs function to add/remove large text class
+
+		 var buttonSubmit = document.getElementById("submit");	// Grabs submit button
+		 buttonSubmit.addEventListener("click", msgSubmit);			// Listens for click; runs function to begin the process of creating a new message
+
+		 var buttonClearAll = document.getElementById("clear");	// Grabs clear button
+		 buttonClearAll.addEventListener("click", msgClearAll);	// Listens for click; runs function to begin the process of clearing all messages
+
+	// Main content elements
+		 var msgArea = document.getElementById("msgArea");			// Grabs the output div where all messages appear
+		 msgArea.addEventListener("click", msgDelete);					// Listens for click; runs function to check event target,
+		 																														// If needed, calls method to delete
+
