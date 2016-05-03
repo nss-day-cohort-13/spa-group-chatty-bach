01b8fbe9c09037bb6da92078cdaff504e4d0f267
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-05-03 17:41:47 -0500

    added 2 functions
    added a function for addNewMessage & for getMessages

---
+++ bwMessage.js
@@ -0,0 +1,19 @@
+var chatty = (function{
+var messages = [];
+
+	return {
+		addNewMessage(newItem) {
+			for (var i = 0; i < return messages.length; i++) {
+				return newItem[i];
+				messages.push(newItem[i]);
+			}
+		}
+
+		newItem.getMessages = function() {
+			return messages;
+		};
+
+	}
+};
+
+
