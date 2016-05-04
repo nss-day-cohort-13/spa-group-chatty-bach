03f1f61b64aeba5cc1e4f90b183e84e390c24e63
Alan Eatherly <eatherlyae@gmail.com>
2016-05-04 10:07:16 -0500

    fix typos on delete iife

--- deleteAll.js
+++ deleteAll.js
@@ -2,15 +2,16 @@ var Chatty = (function (aug) {
 
   aug.deleteMsg = function () {
 
-   var delete = getElementsByClassName("delete")
+   var delete = getElementsByClassName("delete");
 
-    for (var i = 0; i < delete.length; i++)
+    for (var i = 0; i < delete.length; i++) {
 
       if (clickEvent.target === delete[i]) {
         msgArea.removeChild(event.target.parentNode);
         Chatty.getMessages();
         messages.shift(messages[i]);
       }
+    }
   }
 
   return aug;
