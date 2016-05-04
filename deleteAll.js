23da23a59ce9fbcbf2fb2f13f5459cef58cd1817
Alan Eatherly <eatherlyae@gmail.com>
2016-05-04 10:05:48 -0500

    refine delete iife

--- deleteAll.js
+++ deleteAll.js
@@ -1,12 +1,18 @@
-var Chatty = (function () {
+var Chatty = (function (aug) {
+
+  aug.deleteMsg = function () {
 
    var delete = getElementsByClassName("delete")
+
     for (var i = 0; i < delete.length; i++)
+
       if (clickEvent.target === delete[i]) {
         msgArea.removeChild(event.target.parentNode);
         Chatty.getMessages();
         messages.shift(messages[i]);
       }
+  }
 
+  return aug;
 
 }(Chatty || {}));
