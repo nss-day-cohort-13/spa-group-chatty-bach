bc133221f609c8d280ca048921b1a36cbe92b2b4
Hannah Hall <hannahkhall13@gmail.com>
2016-05-18 13:24:11 -0500

    handle to load

--- multipleUsers.js
+++
@@ -1,7 +0,0 @@
-// Create an object in your JavaScript that holds an array of names (see example below).
-var users = {
-	names: ["Xavier", "Joanna", "Mackenzie", "Gunter", "Iveta", "Sven"];
-};
-// Next to the message input box, there should be a radio button group for each name in the list.
-// When a user enters a message, it should be prepended with the chosen user's name, in bold text.
-// Keep in mind that this will likely change the structure of your JSON file since the pre-loaded messages have to have this information on them.
