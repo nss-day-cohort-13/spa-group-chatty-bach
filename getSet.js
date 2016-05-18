24a5eadbc931054b86c40eb94e40a0209668ffb5
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-05-18 15:13:43 -0500

    update gh-pages

---
+++ getSet.js
@@ -0,0 +1,64 @@
+
+var Chatty = (function(aug) {
+
+	var messages = [];
+	var idCounter = 0;
+
+	var outputDiv = document.getElementById("msgArea");
+
+	function Message (string, handle, person, updateTime) {
+		this.string = string;
+		this.handle = handle;
+		this.user = person;
+		this.time = updateTime;
+	}
+
+
+	aug.addNewMessage = function (newItem, person) {
+		var updateTime = new Date();
+		updateTime = updateTime.toLocaleTimeString() + " " + updateTime.toLocaleDateString();
+		var newMsg = new Message(newItem, `msg${idCounter}`, person, updateTime);
+		messages.push(newMsg);
+		idCounter++;
+		Chatty.loadMessages();
+	}
+
+	aug.editMessage = function (editText, editId, index, person) {
+		var updateTime = new Date();
+		updateTime = updateTime.toLocaleTimeString() + " " + updateTime.toLocaleDateString();
+		var editMsg = new Message(editText, editId, person, updateTime);
+		messages[index]=editMsg;
+		Chatty.loadMessages();
+	}
+
+	aug.getMessages = function () {
+		return messages;
+	}
+
+	aug.deleteData = function (ex) {
+		messages = messages.filter(function(message) {
+			return message.handle !== ex;
+		});
+		Chatty.loadMessages();
+}
+
+	aug.loadMessages = function () {
+		var buildHTML = "";
+		if (messages.length >= 20){
+			messages.shift(messages[0]);
+		};
+		for (var i = 0; i < messages.length; i++) {
+			messages[i].handle = `msg${i}`;
+			buildHTML += `<p id="${messages[i].handle}" class="message"><span class="strong">${messages[i].user}</span><label class='userMsg>'>${messages[i].string} </label><button class="edit">Edit</button><button class="delete">Delete</button><span class="timeStamp">${messages[i].time}</span></p>`;
+
+		}
+		outputDiv.innerHTML = buildHTML;
+		for (var i = 0; i < messages.length; i++) {
+			document.getElementById(`${messages[i].handle}`).addEventListener("click", Chatty.deleteMsg);
+		}
+	}
+
+
+	return aug;
+
+}(Chatty || {}));
---
+++ xhr.js
@@ -0,0 +1,25 @@
+var Chatty= (function(xhr){
+
+	var jsonMessages= [];
+	var loadJson = new XMLHttpRequest();
+	loadJson.addEventListener("load", fetchMsg);
+	loadJson.open("GET", "messages.json");
+	loadJson.send();
+
+	function fetchMsg () {
+		var data= JSON.parse(this.responseText);
+
+		for (i = 0; i < data.messages.length; i++) {
+			jsonMessages.push(data.messages[i]);
+			Chatty.addNewMessage(jsonMessages[i].message, jsonMessages[i].user);
+		}
+	}
+
+	xhr.getJson= function () {
+		return jsonMessages;
+	}
+
+	return xhr;
+
+}(Chatty || {}));
+
